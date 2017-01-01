42b357de3bc15508e6d1d327cc8aedbe7fcf803a
Ruben Gil <gil.r419@gmail.com>
2017-01-01 14:48:40 -0500

    Map component is now a stateless component

--- frontend/components/Map.jsx
+++ frontend/components/Map.jsx
@@ -1,46 +1,9 @@
 import React from 'react';
 
-const Map = React.createClass({
-  // getInitialState(){
-  //   return {map: null}
-  // },
-  componentDidMount() {
-
-    // var map = new google.maps.Map(document.getElementById('map'), {
-    //   zoom: 15,
-    //   center: new google.maps.LatLng(this.props.lat, this.props.lng),
-    //   mapTypeId: 'terrain'
-    // });
-    //
-    //
-    // var latLng = new google.maps.LatLng(this.props.lat, this.props.lng);
-    // var marker = new google.maps.Marker({
-    //   position: latLng,
-    //   map: map
-    // });
-    //
-    // this.setState({map: map});
-  },
-  // componentWillReceiveProps(props) {
-  //   var map = new google.maps.Map(document.getElementById('map'), {
-  //     zoom: 15,
-  //     center: new google.maps.LatLng(props.lat, props.lng),
-  //     mapTypeId: 'terrain'
-  //   });
-  //
-  //   var latLng = new google.maps.LatLng(this.props.lat, this.props.lng);
-  //   var marker = new google.maps.Marker({
-  //     position: latLng,
-  //     map: map
-  //   });
-  // },
-  render() {
-    return (
-      <div>
-        <div id="map"></div>
-      </div>
-    )
-  }
-})
+const Map = (props) => (
+  <div>
+    <div id="map"></div>
+  </div>
+)
 
 export default Map;
